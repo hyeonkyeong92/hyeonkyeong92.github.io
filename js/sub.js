eba83c784a1044b7f85b0b5d2b3c918b70d2d617
hyeonkyeong <ddd40510@gmail.com>
2022-08-14 19:02:26 +0900

    0814_animationSection-javascript 구현 진행중

--- js/sub.js
+++ js/sub.js
@@ -1,47 +1,3 @@
-// Store the meta element
-var viewport_meta = document.getElementById('viewport-meta');
-
-// Define our viewport meta values
-var viewports = {
-		default: viewport_meta.getAttribute('content'),
-		landscape: 'width=990'
-	};
-
-// Change the viewport value based on screen.width
-var viewport_set = function() {
-		if ( screen.width > 768 )
-			viewport_meta.setAttribute( 'content', viewports.landscape );
-		else
-			viewport_meta.setAttribute( 'content', viewports.default );
-	}
-
-// Set the correct viewport value on page load
-viewport_set();
-
-// Set the correct viewport after device orientation change or resize
-window.onresize = function() { 
-	viewport_set(); 
-}
-
-
-//==============Button Click Event
-
-// var nav = document.getElementsByClassName('nav-wrap');
-// function openNav(){
-// 	nav.addClass('openNavAction');
-// }
-
-const html = document.querySelector('html');
-const headerBtn = html.querySelector('.toggler');
-
-const navWrap = document.querySelector('.nav-wrap');
-const headerBtnWrap = document.querySelector('.h-menu');
-
-headerBtn.addEventListener('click',() =>{
-	navWrap.classList.toggle('active');
-	headerBtnWrap.classList.toggle('active');
-});
-
 
 //==============Scroll Event
 
@@ -72,13 +28,13 @@ function isElementUnderBottom(elem, triggerDiff) {
     elems2.forEach(elem => {
         var topMove = 0;
         if (isElementUnderBottom(elem, -20)) {
-            topMove = topMove + 2;
-            elem.style.transform = 'translate3d(0,70px,0)';
+            topMove = topMove + 12;
+            elem.style.transform = 'translate3d('+ - topMove +'px,0px,0px)';
             // elem.style.transform = 'translate3d(' + -topMove + ' px, 0px,  0px)';
   
         } else {
-            topMove = topMove - 2;
-            elem.style.transform = 'translate3d(0,0px,0)';
+            topMove = topMove - 12;
+            elem.style.transform = 'translate3d('+ - topMove +'px,0px,0px)';
             // elem.style.transform = 'translate3d(' + -topMove + ' px, 0px,  0px)';
   
         }
