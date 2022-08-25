deda0ec1250316c23f795f5e8b8acb885df3a068
hyeonkyeong <ddd40510@gmail.com>
2022-08-25 18:01:23 +0900

    0825

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 // Store the meta element
-var viewport_meta = document.getElementById('viewport-meta');
+// var viewport_meta = document.getElementById('viewport-meta');
 
 // Define our viewport meta values
-var viewports = {
-		default: viewport_meta.getAttribute('content'),
-		landscape: 'width=990'
-	};
+// var viewports = {
+// 		default: viewport_meta.getAttribute('content'),
+// 		landscape: 'width=990'
+// 	};
 
 // Change the viewport value based on screen.width
-var viewport_set = function() {
-		if ( screen.width > 768 )
-			viewport_meta.setAttribute( 'content', viewports.landscape );
-		else
-			viewport_meta.setAttribute( 'content', viewports.default );
-	}
+// var viewport_set = function() {
+// 		if ( screen.width > 768 )
+// 			viewport_meta.setAttribute( 'content', viewports.landscape );
+// 		else
+// 			viewport_meta.setAttribute( 'content', viewports.default );
+// 	}
 
 // Set the correct viewport value on page load
-viewport_set();
+// viewport_set();
 
 // Set the correct viewport after device orientation change or resize
-window.onresize = function() { 
-	viewport_set(); 
-}
+// window.onresize = function() { 
+// 	viewport_set(); 
+// }
 
 
 //=====================Nav Button Click Event=====================//
@@ -171,6 +171,6 @@ $(window).scroll(function(event){
 
    }
    lastScrollTop = st;
-   console.log(st);
+//    console.log(st);
 });
 
