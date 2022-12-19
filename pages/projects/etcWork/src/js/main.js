a3118a9e937a9eec9ec8a7d411bcd8b47b70a688
hyeonkyeong <ddd40510@gmail.com>
2022-12-19 14:56:02 +0900

    1219-3

--- pages/projects/etcWork/src/js/main.js
+++ pages/projects/etcWork/src/js/main.js
@@ -1,9 +1,43 @@
 
+//탭메뉴
+(function(){
 
-// (()=>{
-//     var pageOn = document.querySelectorAll('.page-on');
-//     console.log(pageOn);
-    
-//     pageOn.classList.add('.font-title');
+    const tabSelectList = document.querySelectorAll('.tab-wrap li');
+    const tabSelectedElem = document.querySelectorAll('.project-lists .item');
+    let activeConts;
 
-// })();
+    // console.log(tabSelectList);
+    // console.log(tabSelectedElem);
+
+    for(var i = 0; i<tabSelectList.length; i++){
+        tabSelectList[i].querySelector('a').addEventListener('click',function(e){
+            e.preventDefault();
+            
+            for(var j = 0; j<tabSelectList.length; j++){
+                tabSelectList[j].classList.remove('on');
+                tabSelectedElem[j].style.display = "none";
+            }
+
+            this.parentNode.classList.add('on');
+
+            activeConts = this.getAttribute('href');
+            document.querySelector(activeConts).style.display = 'flex';
+            document.querySelector(activeConts).style.animation = 'ani-up .8s cubic-bezier(0.51, 0.09, 0.56, 0.89)';
+        });
+
+    }
+
+    document.querySelector('.project-lists .item:nth-child(1)').style.display = 'flex';
+            
+
+})();
+
+
+
+//=========================Page Back=========================//
+const iconBackElem = document.querySelector('.h-menu');
+
+iconBackElem.addEventListener('click', function (){
+    window.history.back();
+    // console.log('click');
+});
