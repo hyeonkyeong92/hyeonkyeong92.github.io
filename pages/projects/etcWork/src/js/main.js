2403c52d144d827ed5c6000683c2e3dfc07486f2
hyeonkyeong <ddd40510@gmail.com>
2023-01-02 18:39:27 +0900

    230102

--- pages/projects/etcWork/src/js/main.js
+++ pages/projects/etcWork/src/js/main.js
@@ -3,11 +3,13 @@
 (function(){
 
     const tabSelectList = document.querySelectorAll('.tab-wrap li');
-    const tabSelectedElem = document.querySelectorAll('.project-lists .item');
+    const tabSelectedElem = document.querySelectorAll('.project-lists .lists');
+    const tabSelectListText =document.querySelectorAll('.tab-wrap li a');
     let activeConts;
 
-    // console.log(tabSelectList);
-    // console.log(tabSelectedElem);
+    console.log(tabSelectList);
+    console.log(tabSelectedElem);
+    console.log(tabSelectListText);
 
     for(var i = 0; i<tabSelectList.length; i++){
         tabSelectList[i].querySelector('a').addEventListener('click',function(e){
@@ -18,20 +20,24 @@
                 tabSelectedElem[j].style.display = "none";
             }
 
+            for(var h = 0; h<tabSelectListText.length; h++){
+                tabSelectListText[h].classList.remove('font-title');
+            }
+
             this.parentNode.classList.add('on');
+            this.classList.add('font-title');
 
             activeConts = this.getAttribute('href');
-            document.querySelector(activeConts).style.display = 'flex';
+            document.querySelector(activeConts).style.display = 'block';
             document.querySelector(activeConts).style.animation = 'ani-up .8s cubic-bezier(0.51, 0.09, 0.56, 0.89)';
         });
 
     }
 
-    document.querySelector('.project-lists .item:nth-child(1)').style.display = 'flex';
+    document.querySelector('.project-lists .item:nth-child(1)').style.display = 'block';
             
 
 })();
-
 
 
 //=========================Page Back=========================//
